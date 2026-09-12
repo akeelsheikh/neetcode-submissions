f6f21d1ada295dd91c0782007350916d7a6e317d
Akeel Ahmad <akeelcare@gmail.com>
2026-09-12 19:44:43 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int i, int n, vector<int>&dp){
+        if(i==n){
+            return 1;
+        }
+        if(i>n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int inc=solve(i+1,n,dp);
+        int exc=solve(i+2,n,dp);
+
+        return dp[i]=inc+exc;
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(0,n,dp);
+    }
+};
